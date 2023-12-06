d3e00f245d1adf26cdb8830cde3f3ec3f18b488e
isaacgichuhi <zacmuturi45@gmail.com>
2023-12-06 17:33:54 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        
+    def match(self, listy):
+        new_word = []
+        clean_word = []
+        m = 0
+        while m < len(listy):
+            for wor in self.word:
+                if wor in listy[m]:
+                    new_word.append(wor)
+            if len(new_word) == len(self.word):
+                clean_word.append(listy[m])
+            new_word = []
+            m = m + 1
+        return clean_word
